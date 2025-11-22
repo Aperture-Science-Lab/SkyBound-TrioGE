35b869a02e3161709eb968dac088b651c20ca810
itsfadymate <fadybassem263@gmail.com>
2025-11-22 10:12:34 +0200

    refactored OpenGLMeshloader to use tha gamemanager

--- OpenGLMeshLoader.cpp
+++ OpenGLMeshLoader.cpp
@@ -3,39 +3,23 @@
 #include "TextureBuilder.h"
 #include "Model_3DS.h"
 #include "GLTexture.h"
-#include "FlightController.h"
+#include "GameManager.h"
+#include "Level2.h"
 #include <Vector3f.h>
 #include <glut.h>
 
-FlightController* flightSim;
 int lastTime = 0;
 
 int WIDTH = 1280;
 int HEIGHT = 720;
 
-GLuint tex;
-char title[] = "3D Model Loader Sample";
+char title[] = "SkyBound - Flight Simulator";
 
 // 3D Projection Options
 GLdouble fovy = 45.0;
 GLdouble aspectRatio = (GLdouble)WIDTH / (GLdouble)HEIGHT;
 GLdouble zNear = 0.1;
 GLdouble zFar = 1000;
-
-
-
-Vector3f Eye(20, 5, 20);
-Vector3f At(0, 0, 0);
-Vector3f Up(0, 1, 0);
-
-int cameraZoom = 0;
-
-// Model Variables
-Model_3DS model_house;
-Model_3DS model_tree;
-
-// Textures
-GLTexture tex_ground;
 
 //=======================================================================
 // Lighting Configuration Function
@@ -95,123 +79,12 @@ void myInit(void)
 }
 
 //=======================================================================
-// Render Ground Function
-//=======================================================================
-void RenderGround()
-{
-	glDisable(GL_LIGHTING);
-	glColor3f(0.6f, 0.6f, 0.6f);
-	glEnable(GL_TEXTURE_2D);
-	glBindTexture(GL_TEXTURE_2D, tex_ground.texture[0]);
-    
-    // Ensure texture repeats
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-
-	glPushMatrix();
-    
-    // Infinite Ground Logic
-    float px = 0, pz = 0;
-    if (flightSim) {
-        px = flightSim->player.position.x;
-        pz = flightSim->player.position.z;
-    }
-    
-    // Move ground with player
-    glTranslatef(px, 0, pz);
-    
-    float size = 2000.0f; // Larger than zFar
-    float texScale = 0.05f; // Adjust for texture density
-
-	glBegin(GL_QUADS);
-	glNormal3f(0, 1, 0);
-    
-    // Calculate texture coordinates based on world position to simulate movement
-	glTexCoord2f((px - size) * texScale, (pz - size) * texScale); 
-    glVertex3f(-size, 0, -size);
-    
-	glTexCoord2f((px + size) * texScale, (pz - size) * texScale); 
-    glVertex3f(size, 0, -size);
-    
-	glTexCoord2f((px + size) * texScale, (pz + size) * texScale); 
-    glVertex3f(size, 0, size);
-    
-	glTexCoord2f((px - size) * texScale, (pz + size) * texScale); 
-    glVertex3f(-size, 0, size);
-    
-	glEnd();
-	glPopMatrix();
-
-	glEnable(GL_LIGHTING);
-	glColor3f(1, 1, 1);
-}
-
-//=======================================================================
 // Display Function
 //=======================================================================
 void myDisplay(void)
 {
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-
-    // Camera Setup from FlightController
-    if (flightSim) {
-        flightSim->setupCamera();
-    }
-
-	// Sky box
-	glPushMatrix();
-	GLUquadricObj* qobj;
-	qobj = gluNewQuadric();
-	
-    // Move skybox with camera to make it appear infinite
-    if (flightSim) {
-        glTranslatef(flightSim->player.position.x, flightSim->player.position.y, flightSim->player.position.z);
-    }
-
-	glRotated(90, 1, 0, 1);
-	glBindTexture(GL_TEXTURE_2D, tex);
-	gluQuadricTexture(qobj, true);
-	gluQuadricNormals(qobj, GL_SMOOTH);
-    
-    // Disable depth write so it's always background
-    glDepthMask(GL_FALSE);
-    glDisable(GL_LIGHTING); // Disable lighting for skybox
-	gluSphere(qobj, 800, 100, 100); 
-    glEnable(GL_LIGHTING);  // Re-enable lighting
-    glDepthMask(GL_TRUE);
-    
-	gluDeleteQuadric(qobj);
-	glPopMatrix();
-
-	GLfloat lightIntensity[] = { 0.7f, 0.7f, 0.7f, 1.0f };
-	GLfloat lightPosition[] = { 0.0f, 100.0f, 0.0f, 0.0f };
-	glLightfv(GL_LIGHT0, GL_POSITION, lightPosition);
-	glLightfv(GL_LIGHT0, GL_AMBIENT, lightIntensity);
-
-	RenderGround();
-
-    // Draw Plane
-    if (flightSim) {
-        flightSim->drawPlane();
-    }
-
-	// Draw Tree Model
-	glPushMatrix();
-	glTranslatef(10, 0, 0);
-	glScalef(0.7f, 0.7f, 0.7f);
-	model_tree.Draw();
-	glPopMatrix();
-
-	// Draw House Model
-	glPushMatrix();
-	glRotatef(90.f, 1, 0, 0);
-	model_house.Draw();
-	glPopMatrix();
-
-	glutSwapBuffers();
+	// Delegate rendering to GameManager
+	GameManager::getInstance().render();
 }
 
 //=======================================================================
@@ -221,18 +94,9 @@ void Anim() {
     int currentTime = glutGet(GLUT_ELAPSED_TIME);
     float deltaTime = (currentTime - lastTime) / 1000.0f;
     lastTime = currentTime;
-    if (deltaTime > 0.1f) deltaTime = 0.1f;
-
-    if (flightSim) {
-        flightSim->update(deltaTime);
-    }
     
-    // Center mouse
-    if (WIDTH > 0 && HEIGHT > 0) {
-        int centerX = WIDTH / 2;
-        int centerY = HEIGHT / 2;
-        glutWarpPointer(centerX, centerY);
-    }
+    // Update through GameManager
+    GameManager::getInstance().update(deltaTime);
 
     glutPostRedisplay();
 }
@@ -242,17 +106,13 @@ void Anim() {
 //=======================================================================
 void myKeyboard(unsigned char button, int x, int y)
 {
-    if (button == 27) exit(0);
-    
-    if (flightSim) {
-        flightSim->handleInput(button, true);
-    }
+    // Forward input to GameManager
+    GameManager::getInstance().handleKeyboard(button, true);
 }
 
 void myKeyboardUp(unsigned char button, int x, int y) {
-    if (flightSim) {
-        flightSim->handleInput(button, false);
-    }
+    // Forward input to GameManager
+    GameManager::getInstance().handleKeyboardUp(button);
 }
 
 //=======================================================================
@@ -260,11 +120,8 @@ void myKeyboardUp(unsigned char button, int x, int y) {
 //=======================================================================
 void myMotion(int x, int y)
 {
-    int centerX = WIDTH / 2;
-    int centerY = HEIGHT / 2;
-    if (flightSim) {
-        flightSim->handleMouse(x, y, centerX, centerY);
-    }
+    // Forward mouse input to GameManager
+    GameManager::getInstance().handleMouse(x, y);
 }
 
 //=======================================================================
@@ -287,21 +144,6 @@ void myReshape(int w, int h)
 }
 
 //=======================================================================
-// Assets Loading Function
-//=======================================================================
-void LoadAssets()
-{
-	model_house.Load("Models/house/house.3DS");
-	model_tree.Load("Models/tree/Tree1.3ds");
-	tex_ground.Load("Textures/ground.bmp");
-	loadBMP(&tex, "Textures/blu-sky-3.bmp", true);
-    
-    if (flightSim) {
-        flightSim->loadModel("models/plane/mitsubishi_a6m2_zero_model_11.3ds");
-    }
-}
-
-//=======================================================================
 // Main Function
 //=======================================================================
 int main(int argc, char** argv)
@@ -321,14 +163,23 @@ int main(int argc, char** argv)
     glutPassiveMotionFunc(myMotion);
     glutMotionFunc(myMotion);
     glutIdleFunc(Anim);
-    
-    glutSetCursor(GLUT_CURSOR_NONE);
 
 	myInit();
     
-    flightSim = new FlightController();
-	LoadAssets();
+    // Initialize GameManager and register levels
+    Level2* flightLevel = new Level2();
+    GameManager::getInstance().registerLevel("FlightSimulator", flightLevel);
+    
+    // Initialize and switch to the first level
+    flightLevel->init();
+    GameManager::getInstance().switchToLevel("FlightSimulator");
+    
+    lastTime = glutGet(GLUT_ELAPSED_TIME);
 
 	glutMainLoop();
+    
+    // Cleanup
+    GameManager::getInstance().cleanup();
+    
     return 0;
 }
